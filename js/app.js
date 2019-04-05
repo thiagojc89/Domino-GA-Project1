70f1c27024225e725f1203e494f3457a8a3fe18f
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-04-05 18:37:50 -0500

    when two dominos gets snap we know their values

--- js/app.js
+++ js/app.js
@@ -1,8 +1,59 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $( ".domino" ).draggable();
-$( ".domino" ).draggable( "option", "snap", true);
-$( "#66" ).css("transform", "rotate(90deg)");
+// $( ".domino" ).draggable( "option", "snapMode", "outer");
+// $( "#65" ).css("transform", "rotate(90deg)");
+// $( "#54" ).css("transform", "rotate(90deg)");
+
+$(".domino")
+.mouseup(function() {
+	console.log("mouse up");
+	console.log($(this));
+    $( this ).draggable( "option", "snap", false);
+    console.log("mouse up");
+})
+.mousedown(function() {
+	console.log("mouse down");
+	console.log($(this));
+    $( this ).draggable( "option", "snap", true);
+    $( this ).draggable( "option", "snapMode", "outer");
+    console.log("mouse down");
+});
 
 
+
+
+
+
+$(".domino").draggable({
+    snap: ".domino",
+    stop: function(event, ui) { 
+        /* Get the possible snap targets: */
+        var snapped = $(this).data('uiDraggable').snapElements;
+       
+        /* Pull out only the snap targets that are "snapping": */
+        var snappedTo = $.map(snapped, function(element) {
+            return element.snapping ? element.item : null;
+        });
+
+        console.log(snappedTo);
+       
+        
+    }
+});
 
 
 
