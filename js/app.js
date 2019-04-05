761e54c5ced3b164cf8cf20cb5d3f3ac78e4b0cc
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-04-05 18:55:57 -0500

    starting creating the logic to instatiate the dominos objects

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+/*for (let i = 0; i < 7; i++){
+	for (let j = i; j < 7; j++)
+		console.log(i,j);
 
-
+	// const
+}
+*/
 
 
 
@@ -43,12 +48,12 @@ $(".domino").draggable({
     stop: function(event, ui) { 
         /* Get the possible snap targets: */
         var snapped = $(this).data('uiDraggable').snapElements;
-       
+       	console.log(snapped);
         /* Pull out only the snap targets that are "snapping": */
         var snappedTo = $.map(snapped, function(element) {
             return element.snapping ? element.item : null;
         });
-
+        
         console.log(snappedTo);
        
         
